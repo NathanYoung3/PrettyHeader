66c5c4fdaa050d027c74db0153fb9a09c02f9c44
Natedawg2077 <NatedawgGDEV@gmail.com>
2024-11-13 08:55:44 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        InputHelper.prettyHeader("ThIs Is NoT cEnTeReD1!1!1!");
+    }
+}
